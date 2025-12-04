df1f0e0f92a7259c7eab5489a9bb4f37ad44830d
morteza1984 <1809764+morteza1984@users.noreply.github.com>
2025-12-04 16:54:04 +0100

    init

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    """Testet, ob die Home-Route funktioniert"""
+    rv = client.get('/')
+    assert rv.status_code == 200
+    assert b"Hello CI/CD" in rv.data
