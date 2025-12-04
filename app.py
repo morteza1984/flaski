fca3362b29eb788ee25169a56bfb886da9bfafae
morteza1984 <1809764+morteza1984@users.noreply.github.com>
2025-12-04 18:01:08 +0100

    change

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return "Hello CI/CD", 200
+
+
+def my_sum(a: int, b: int) -> int:
+    return a+b
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,6 @@
 import pytest
-from app import app
+from app import app, my_sum
+
 
 @pytest.fixture
 def client():
@@ -7,8 +8,23 @@ def client():
     with app.test_client() as client:
         yield client
 
+
 def test_home(client):
     """Testet, ob die Home-Route funktioniert"""
     rv = client.get('/')
     assert rv.status_code == 200
     assert b"Hello CI/CD" in rv.data
+
+
+def test_sum():
+    result = my_sum(5, 5)
+    assert result == 10
+    assert my_sum(-5, 5) == 0
+    assert my_sum(0, 0) == 0
+
+
+def test_multi():
+    result = my_sum(5, 5)
+    assert result == 10
+    assert my_sum(-5, 5) == 0
+    assert my_sum(0, 0) == 0
