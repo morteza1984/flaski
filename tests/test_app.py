fe4b41f86fc26b87a483adf6803aee6d42cae012
morteza1984 <1809764+morteza1984@users.noreply.github.com>
2025-12-04 18:20:53 +0100

    remove

--- tests/test_app.py
+++ tests/test_app.py
@@ -14,17 +14,3 @@ def test_home(client):
     rv = client.get('/')
     assert rv.status_code == 200
     assert b"Hello CI/CD" in rv.data
-
-
-def test_sum():
-    result = my_sum(5, 5)
-    assert result == 10
-    assert my_sum(-5, 5) == 0
-    assert my_sum(0, 0) == 0
-
-
-def test_multi():
-    result = my_sum1(5, 5)
-    assert result == 10
-    assert my_sum(-5, 5) == 0
-    assert my_sum(0, 0) == 0
